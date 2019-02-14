279e70823566db49c3dc6d7a8e2ac5230f30d840
Dinesh <d.hebbar7@gmail.com>
2019-02-14 12:56:12 +0530

    layouts added

--- app/src/main/java/com/dineshhebbar/navigationdrawer/activity/MainActivity.java
+++ app/src/main/java/com/dineshhebbar/navigationdrawer/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.dineshhebbar.navigationdrawer;
+package com.dineshhebbar.navigationdrawer.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.dineshhebbar.navigationdrawer.R;
 
 public class MainActivity extends AppCompatActivity {
     TextView hello;
