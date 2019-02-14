91a897671d1077da1df580cc240f7d641924e9f3
Dinesh <d.hebbar7@gmail.com>
2019-02-14 18:03:19 +0530

    home

--- app/src/main/java/com/dineshhebbar/navigationdrawer/activity/MainActivity.java
+++ app/src/main/java/com/dineshhebbar/navigationdrawer/activity/MainActivity.java
@@ -1,16 +1,45 @@
 package com.dineshhebbar.navigationdrawer.activity;
 
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
 import com.dineshhebbar.navigationdrawer.R;
 
 public class MainActivity extends AppCompatActivity {
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private View navHeader;
+    private TextView txtName, txtWebsite;
+    private Toolbar toolbar;
+
+    public static int navItemIndex = 0;
+    // tags used to attach the fragments
+    private static final String TAG_HOME = "home";
+    private static final String TAG_SETTINGS = "settings";
+
+    private String[] activityTitles;
+
+    // flag to load home fragment when user presses back key
+    private boolean shouldLoadHomeFragOnBackPress = true;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mHandler = new Handler();
+
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
     }
 }
